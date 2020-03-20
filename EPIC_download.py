424f5784a31933cb326330cd131ec0ca298fce6a
Trevor Hunsaker <tmxhunsaker@gmail.com>
2020-03-19 21:42:40 -0600

    Added option to download thumbnails

--- EPIC_download.py
+++ EPIC_download.py
@@ -6,48 +6,68 @@ import json
 
 
 
-# EPIC api docs
+# EPIC api 2.0 docs
 # https://epic.gsfc.nasa.gov/about/api
 
 # Maintain a json file with metadata for each image
 SAVE_META = True
 
-# "jpg" for faster download
-# "png" for higher quality
-image_format = "jpg"
+#Options: 'full'  'half'  'thumbs'
+QUALITY = 'half'
+
+
+# Determine query arguments
+if QUALITY == 'full':
+    image_format = 'png'
+    image_type = 'png'
+if QUALITY == 'half':
+    image_format = 'jpg'
+    image_type = 'jpg'
+if QUALITY == 'thumbs':
+    image_format ='jpg'
+    image_type = 'thumbs'
+
 
 # Data storage location is received as a cmdline argument
-
 if len(sys.argv) == 2:
     dataset_folder_location = Path(sys.argv[1])
 else:
     dataset_folder_location = Path.cwd()
 
-image_folder = dataset_folder_location / "EPIC/natural"
+dataset_folder = dataset_folder_location / "EPIC"
 
-# To check for additional images uploaded to
-# previously checked dates, delete acquired_file
-acquired_file = dataset_folder_location / "dates_acquired.json"
+if QUALITY == 'thumbs':
+    image_folder = dataset_folder / "natural_thumbs"
+else:
+    image_folder = dataset_folder / "natural"
+
+
+
+# NOTE: To check for additional images uploaded to
+# previously checked dates, delete dates_acquired.json
+dates_acquired = dataset_folder / "dates_acquired.json"
 
 if SAVE_META:
-    metadata_file = dataset_folder_location / "EPIC_data.json"
+    metadata_file = dataset_folder / "EPIC_data.json"
 
 baseurl = "https://epic.gsfc.nasa.gov/"
 
 if not Path.exists( image_folder ):
-    image_folder .mkdir(parents=True)
+    image_folder.mkdir(parents=True)
 
 # load dates with downloaded images
-if Path.is_file( acquired_file ):
-    with open( acquired_file, 'r' ) as file:
+if dates_acquired.exists():
+    with open( dates_acquired, 'r' ) as file:
         dates = json.loads( file.read() )
+    print("Resuming dataset download...")
 else:
+    print("Beginning dataset download...")
     dates = []
 
 if SAVE_META:
     # load metadata of downloaded images to append to
-    if Path.is_file( dataset_folder_location / metadata_file ):
-        with open( dataset_folder_location / metadata_file, 'r') as file:
+    if Path.is_file( metadata_file ):
+        with open( metadata_file, 'r') as file:
             metadata = json.loads( file.read() )
     else:
         metadata = []
@@ -74,11 +94,11 @@ for date in new_dates:
     for image_data in date_data:
         file_name = image_data[ "image" ] + "." + image_format
         if file_name not in downloaded_images:
-            image_url = baseurl + "archive/natural/" + date.replace('-','/') + '/' + image_format + '/' + file_name
+            image_url = baseurl + "archive/natural/" + date.replace('-','/') + '/' + image_type + '/' + file_name
             if not Path.exists( image_folder / file_name ):
                 print( "Downloading:", file_name )
                 image = requests.get( image_url )
-                with open( image_folder + file_name, 'wb') as file:
+                with open( image_folder / file_name, 'wb') as file:
                     file.write( image.content )
             else:
                 print("Image file found:  " + file_name)
@@ -89,5 +109,5 @@ if SAVE_META:
         json.dump( metadata, file)
 
 # Save dates with acquired data
-with open( acquired_file, 'w') as file:
+with open( dates_acquired, 'w') as file:
     json.dump( dates, file)
